51c9fd4641a1ed722bc4ae7881b0300fbb51c049
DenkCool <93751741+MrArbai@users.noreply.github.com>
2022-02-25 14:14:43 +0700

    Template

--- app/Views/pages/about.php
+++ app/Views/pages/about.php
@@ -1,9 +1,16 @@
+<?= $this->extend('layout/Template'); ?>
+<?= $this->section('content'); ?>
 <div class="container">
     <div class="row">
         <div class="col">
             <h1>About Me</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius tempore fugit doloremque praesentium alias iure explicabo reprehenderit obcaecati quaerat illo dolore quia maiores dolores sed pariatur, nostrum voluptate quisquam fuga?</p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius tempore fugit doloremque praesentium
+                alias iure explicabo reprehenderit obcaecati quaerat illo dolore quia maiores dolores sed pariatur,
+                nostrum voluptate quisquam fuga?
+            </p>
 
         </div>
     </div>
 </div>
+<?= $this->endsection(); ?>
