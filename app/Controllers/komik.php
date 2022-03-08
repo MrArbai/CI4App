ab34c2906d125c6b8c3716c644979b81da98ebc1
arbai <mrarbai4@gmail.com>
2022-03-08 23:58:53 +0700

    inser data

--- app/Controllers/komik.php
+++ app/Controllers/komik.php
@@ -30,6 +30,11 @@ class komik extends BaseController
             'title' => 'Daftar Komik',
             'komik' => $this->KomikModel->getKomik($slug)
         ];
+        if (empty($data['komik'])){
+
+            throw \CodeIgniter\Exceptions\PageNotFoundException::forPageNotFound(' Judul Komik ' . $slug . ' Tidak di Temukan ');
+            
+        }
         return view('komik/detail', $data);
     }
 
@@ -39,5 +44,22 @@ class komik extends BaseController
             'title' => 'Tambah Data Komik'
         ];
         return view('komik/create', $data);
+    }
+
+    public function Save()
+    {
+        // dd($this->request->getvar());
+        $slug = url_title($this->request->getvar('judul'),'-',true);
+        $this->KomikModel->save([
+            'Judul' => $this->request->getvar('judul'),
+            'Slug' => $slug,
+            'Penulis' => $this->request->getvar('penulis'),
+            'Penerbit' => $this->request->getvar('penerbit'),
+            'Sampul' => $this->request->getvar('sampul')
+        ]);
+
+        session()->setflashdata('pesan','Data berhasil Di Simpan');
+        
+        return redirect()->to('/komik');
     }
 }
