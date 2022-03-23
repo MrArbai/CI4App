c63f92b2582b3c7dd178b45da9944f9c831805f8
DenkCool <93751741+MrArbai@users.noreply.github.com>
2022-03-23 17:05:50 +0700

    update

--- app/Views/adminDesa/bg_login.php
+++ app/Views/adminDesa/bg_login.php
@@ -1,103 +1,44 @@
-<!DOCTYPE html>
-<html lang="en">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 
 <head>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>SB Admin 2 - Login</title>
-
-    <!-- Custom fonts for this template-->
-    <link href="/css/all.min.css" rel="stylesheet" type="text/css">
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
-
-    <!-- Custom styles for this template-->
-    <link href="/css/sb-admin-2.min.css" rel="stylesheet">
-
+    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+    <title>Administrator Log In</title>
+    <link href="/css/style.css" rel="stylesheet" type="text/css" />
 </head>
 
-<body class="bg-gradient-primary">
-
-    <div class="container">
-
-        <!-- Outer Row -->
-        <div class="row justify-content-center">
-
-            <div class="col-xl-10 col-lg-12 col-md-9">
-
-                <div class="card o-hidden border-0 shadow-lg my-5">
-                    <div class="card-body p-0">
-                        <!-- Nested Row within Card Body -->
-                        <div class="row">
-                            <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
-                            <div class="col-lg-6">
-                                <div class="p-5">
-                                    <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
-                                    </div>
-                                    <form class="user">
-                                        <div class="form-group">
-                                            <input type="email" class="form-control form-control-user"
-                                                id="exampleInputEmail" aria-describedby="emailHelp"
-                                                placeholder="Enter Email Address...">
-                                        </div>
-                                        <div class="form-group">
-                                            <input type="password" class="form-control form-control-user"
-                                                id="exampleInputPassword" placeholder="Password">
-                                        </div>
-                                        <div class="form-group">
-                                            <div class="custom-control custom-checkbox small">
-                                                <input type="checkbox" class="custom-control-input" id="customCheck">
-                                                <label class="custom-control-label" for="customCheck">Remember
-                                                    Me</label>
-                                            </div>
-                                        </div>
-                                        <a href="index.html" class="btn btn-primary btn-user btn-block">
-                                            Login
-                                        </a>
-                                        <hr>
-                                        <a href="index.html" class="btn btn-google btn-user btn-block">
-                                            <i class="fab fa-google fa-fw"></i> Login with Google
-                                        </a>
-                                        <a href="index.html" class="btn btn-facebook btn-user btn-block">
-                                            <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
-                                        </a>
-                                    </form>
-                                    <hr>
-                                    <div class="text-center">
-                                        <a class="small" href="forgot-password.html">Forgot Password?</a>
-                                    </div>
-                                    <div class="text-center">
-                                        <a class="small" href="register.html">Create an Account!</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-
+<body><br /><br /><br /><br /><br />
+    <form method="POST" action="/admin/login">
+        <div id="box-login">
+            <table width="100%">
+                <tr>
+                    <td width="100" colspan="3" align="center">
+                        <img src="/images/inhil-badge.png" alt="">
+                        <h2><a href="">Blog Desa</a></h2>
+                    </td>
+                </tr>
+                <tr>
+                    <td width="100" colspan="3" align="center">
+                    </td>
+                </tr>
+                <tr>
+                    <td width="100">Username</td>
+                    <td width="20">:</td>
+                    <td><input type="text" name="username" class="input" size="40" /></td>
+                </tr>
+                <tr>
+                    <td width="100">Password</td>
+                    <td width="20">:</td>
+                    <td><input type="password" name="password" class="input" size="40" /></td>
+                </tr>
+                <tr>
+                    <td width="100"></td>
+                    <td width="20"></td>
+                    <td><input type="submit" class="tombol" value="Masuk" /> <input type="reset" class="tombol" value="Hapus" /></td>
+                </tr>
+            </table>
         </div>
-
-    </div>
-
-    <!-- Bootstrap core JavaScript-->
-    <script src="/jquery/jquery.min.js"></script>
-    <script src="/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-    <!-- Core plugin JavaScript-->
-    <script src="/jquery-easing/jquery.easing.min.js"></script>
-
-    <!-- Custom scripts for all pages-->
-    <script src="/js/sb-admin-2.min.js"></script>
-
+    </form>
 </body>
 
 </html>
